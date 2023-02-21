95adb0e0094c7c265ad208817abf59c71a7a38b9
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-21 14:44:09 +0100

    Update models/base_model.py: add args ans kwargs method if is not empty

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,18 @@ from datetime import datetime
 
 class BaseModel:
     """Defines the attributes and methods"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes a new instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'update_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representing the instance"""
