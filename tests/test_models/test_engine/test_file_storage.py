68227d39d55db2cd6560cb314dbd670665995d7b
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-02-23 17:32:46 +0100

    add save test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,5 +1,7 @@
 import unittest
 import os.path
+import datetime
+import json
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models import storage
@@ -27,10 +29,11 @@ class TestFileStorage(unittest.TestCase):
         FileStorage.new(FileStorage, model)
         self.assertNotEquals(objects[f"{model.__class__.__name__}.{model.id}"], None)
 
-    def test_reload(self):
-            FileStorage._FileStorage__objects = {}
-            objects = FileStorage._FileStorage__objects
-            obj = objects.copy()
-            model = BaseModel()
-            FileStorage.reload(FileStorage)
-            self.assertNotEqual(obj, objects)
+    def save(self):
+        self.updated_at = datetime.utcnow()
+        with open("file.json", mode="r", encoding="utf-8") as file:
+            data = json.load(file)
+        key = "{}.{}".format(self.__class__.__name__, self.id)
+        data[key] = self.to_dict()
+        with open("file.json", mode="w", encoding="utf-8") as file:
+            json.dump(data, file)
